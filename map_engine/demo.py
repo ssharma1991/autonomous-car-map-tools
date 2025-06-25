27ed40a3a0a46f22683e1c4078cef90cd0b58e03
Shashank Sharma <shashank.sharma.1991@outlook.com>
2025-06-25 03:28:21 -0700

    docs: improve readme

--- map_engine/demo.py
+++ map_engine/demo.py
@@ -3,6 +3,6 @@
 from map_engine import MapEngine
 
 map_engine = MapEngine()
-map_engine.set_gnss_data('../gnss_simulator/demo_virtual_drive.csv')
+map_engine.set_gnss_data('gnss_simulator/demo_virtual_drive.csv')
 map_engine.calculate_realtime_map()
 map_engine.plot_map()
